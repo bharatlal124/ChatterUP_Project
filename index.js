10349dc779247e6e7ad4c553106590e2b8a39ef0
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-11-18 15:25:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ dotenv.config();
 import { connectToDatabase } from "./db.config.js";
 import {server} from './server.js'
 connectToDatabase
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(5100, () => {
+    console.log('Server is running on port 5100');
     // call the connect to database here
     connectToDatabase();
 });
